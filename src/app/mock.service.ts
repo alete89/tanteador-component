fd4a285e3c17a80b6ce84a160c67e46a88b6e4f2
alete89 <maildealete@gmail.com>
2019-02-01 21:29:48 -0300

    service

---
+++ src/app/mock.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Fecha } from 'src/domain/Fecha';
+import { Equipo } from 'src/domain/Equipo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MockService {
+  river: Equipo = new Equipo()
+  boca: Equipo = new Equipo()
+
+  constructor() { }
+
+  async unaFecha() {
+    const fecha = new Fecha()
+    this.river.nombre = "river"
+    this.boca.nombre = "boca"
+    fecha.local = this.river
+    fecha.visitante = this.boca
+    fecha.fechaHoraInicio = new Date(2018, 11, 20, 23, 30)
+    fecha.lugar = "mi casa"
+
+    return fecha
+  }
+
+}
