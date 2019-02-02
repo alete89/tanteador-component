b0b08a942aef763f6c126a37fbaa8babe90fd106
alete89 <maildealete@gmail.com>
2019-02-02 14:17:27 -0300

    service real

---
+++ src/app/services/backend-service.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Equipo } from 'src/domain/Equipo';
+import { Fecha } from 'src/domain/Fecha';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendServiceService {
+  API_URL = "http://127.0.0.1:8080/"
+
+  constructor(private http: Http) {
+  }
+
+  async unEquipo() {
+    const url = this.API_URL + "equipo/"
+    const resp = await this.http.get(url).toPromise()
+    return Equipo.fromJson(resp.json())
+  }
+
+  async unaFecha() {
+    const url = this.API_URL + "unaFecha/"
+    const resp = await this.http.get(url).toPromise()
+    return Fecha.fromJson(resp.json())
+  }
+
+}
