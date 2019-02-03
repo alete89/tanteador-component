ea82ccabd5f8f1dedff70b906351d49ea25d9163
alete89 <maildealete@gmail.com>
2019-02-03 15:47:18 -0300

    constructor con partial

--- src/domain/Equipo.ts
+++ src/domain/Equipo.ts
@@ -2,6 +2,11 @@ export class Equipo {
     nombre: string
     logoUrl: string
 
+
+    constructor(init?: Partial<Equipo>) {
+        Object.assign(this, init)
+    }
+
     static fromJson(EquipoJson) {
         return Object.assign(new Equipo(), EquipoJson)
     }
