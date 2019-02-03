25b1dc936cfb57842abd46a662190b2cc1a20d73
alete89 <maildealete@gmail.com>
2019-02-03 01:32:19 -0300

    una fecha tiene los resultados (considerar arrays?)

--- src/domain/Fecha.ts
+++ src/domain/Fecha.ts
@@ -6,6 +6,23 @@ export class Fecha {
     fechaHoraInicio: Date
     lugar: string
 
+    iniciado: boolean = false
+    setsGanadosLocal: number = 0
+    setsGanadosVisitante: number = 0
+
+    set1local: number = 0
+    set2local: number = 0
+    set3local: number = 0
+    set4local: number = 0
+    set5local: number = 0
+
+    set1visitante: number = 0
+    set2visitante: number = 0
+    set3visitante: number = 0
+    set4visitante: number = 0
+    set5visitante: number = 0
+
+
     static fromJson(fechaJson) {
         var nuevaFecha = Object.assign(new Equipo(), fechaJson)
         nuevaFecha.fechaHoraInicio = new Date(fechaJson.fechaHoraInicio)
